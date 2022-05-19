a32ced9148768df1782e40a64122cf3ebe4cc295
mohzakaria <95968167+mohzakaria@users.noreply.github.com>
2022-05-19 15:40:02 +0200

    Add files via upload

---
+++ PromotionEvents.h
@@ -0,0 +1,17 @@
+#pragma once
+#include "Events.h"
+
+
+class Events;
+class PromotionEvents : public Events
+{
+private:
+    int cost;
+public:
+    PromotionEvents();
+    ~PromotionEvents();
+    PromotionEvents(int etd, int ethr, int id , int money);
+    void execute(Company* pcompany);
+    int getCost();
+};
+
